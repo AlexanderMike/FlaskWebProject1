23954e34b2a518df241b7f93202ab8dce80b52d1
malexander <malexander@gmail.com>
2016-10-03 12:03:30 -0600

    Added webhook page with request.method

--- FlaskWebProject1/FlaskWebProject1/views.py
+++ FlaskWebProject1/FlaskWebProject1/views.py
@@ -26,15 +26,27 @@ def contact():
         year=datetime.now().year,
         message='Your contact page.'
     )
-
-@app.route('/ti_webhook_cp', methods = ['POST'])
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+@app.route('/ti_webhook_cp', methods = ['GET','POST'])
 def ti_webhook_cp():
     """Renders the webhook receiver for course purchased page."""
+    if request.method == 'POST':
+        ti_webhook2 = request.get_json(force=True)
+    else:
+        ti_webhook2 = "No webhook received"
     return render_template(
         'TI_webhook_cp.html',
         title='Webhook',
         year=datetime.now().year,
-        message='Your application description page.'
-        ti_webhook = request.get_json()
+        message='Your application description page.',
+        ti_webhook = ti_webhook2
     )
 
